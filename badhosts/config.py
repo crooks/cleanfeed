65ca188fc31bf7b3f7e3942a47231299fc00b001
crooks <crooks@e946ef36-3dba-4754-8f9b-822e723fb872>
2008-02-06 18:54:47 +0000

    Couple of corrections so it actually works!
    git-svn-id: file:///var/svn-repos/cleanfeed/cleanfeed/trunk@52 e946ef36-3dba-4754-8f9b-822e723fb872

--- badhosts/config.py
+++ badhosts/config.py
@@ -18,16 +18,17 @@
 
 # Filename for the badhosts database file
 # TODO This needs to be fully qualified for release versions
-dbfile = '/usr/local/news/cleanfeed/etc/badhosts.db'
+dbfile = '/usr/local/news/badhosts/badhosts.db'
 
 # How many rejects we accept from a host before declaring it a bad guy
 threshold = 200
 
-# Hours to retain bad_hosts before expiring them from the db
-expire = 160
+# Hours to retain bad_hosts before expiring them from the db.
+# (One week = 168, Two weeks = 336)
+expire = 336
 
 report_file = '/usr/local/news/cleanfeed/log/badhosts'
 
-badhosts_textfile = '/usr/local/news/badhosts/badhosts.txt'
+badhosts_textfile = '/usr/local/news/cleanfeed/etc/bad_hosts_central'
 
 exclude_file = '/usr/local/news/badhosts/exclude_hosts'
