d25bb7d27522e93f9446bc2e2263367e7db4018c
crooks <crooks@e946ef36-3dba-4754-8f9b-822e723fb872>
2008-04-22 11:14:21 +0000

    Tighten up bin_allowed regex
    git-svn-id: file:///var/svn-repos/cleanfeed/cleanfeed/trunk@95 e946ef36-3dba-4754-8f9b-822e723fb872

--- filter_nnrpd.pl
+++ filter_nnrpd.pl
@@ -2,17 +2,15 @@
 # vim: shiftwidth=4 tabstop=4 autoindent smartindent smarttab
 
 use strict;
+use Digest::MD5;
 our (%hdr, $modify_headers, $user);
 
 sub filter_post {
-    my @delete_headers = qw(NNTP-Posting-Host X-Trace);
-
-    if ($user eq 'md@newsguests') {
-        push(@delete_headers, qw(Sender X-Complaints-To));
-    }
-
-    $hdr{$_} = undef foreach @delete_headers;
+    my $ctx = Digest::MD5->new;
+	$ctx->add($hdr{'NNTP-Posting-Host'});
+	$ctx->add("Welcome to the Jungle");
+    $hdr{'NNTP-Posting-Host'} = $ctx->hexdigest;
+	undef $hdr{'X-Trace'};
     $modify_headers = 1;
-	
     return '';
 }
