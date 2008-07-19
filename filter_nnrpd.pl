27320eb49c0391ebfddcfdfb8ec92ffb64d868c8
cleanfeed <cleanfeed@e946ef36-3dba-4754-8f9b-822e723fb872>
2008-07-19 16:10:00 +0000

    Update the nnrpd filter example
    git-svn-id: file:///var/svn-repos/cleanfeed/cleanfeed/trunk@97 e946ef36-3dba-4754-8f9b-822e723fb872

--- filter_nnrpd.pl
+++ filter_nnrpd.pl
@@ -3,14 +3,29 @@
 
 use strict;
 use Digest::MD5;
-our (%hdr, $modify_headers, $user);
+our (%hdr, $modify_headers);
 
 sub filter_post {
+
+	# Hash the NNTP-Posting-Host header
     my $ctx = Digest::MD5->new;
 	$ctx->add($hdr{'NNTP-Posting-Host'});
-	$ctx->add("Welcome to the Jungle");
+	$ctx->add("A touch of salt");
     $hdr{'NNTP-Posting-Host'} = $ctx->hexdigest;
 	undef $hdr{'X-Trace'};
+
+	# Modify headers for postings from the Reece Mail2News
+	if ($hdr{'Mail-To-News-Contact'} =~ /abuse\@reece\.net\.au/) {
+		undef $hdr{'X-Complaints-To'};
+		$hdr{'Path'} = 'reece.net.au!not-for-mail';
+	};
+
+	# Refuse these From addresses
+	if ($hdr{From} =~/
+			\@darkshado.ca/ix) {
+		return 'Sender address (From) is blacklisted';
+	};
+
     $modify_headers = 1;
     return '';
 }
